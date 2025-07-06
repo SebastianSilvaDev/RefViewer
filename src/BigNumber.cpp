6090ab291e8d16f66da259fb738b44d6e1fdd56e
Sebastian Silva <sebastiansilvabenites@gmail.com>
2025-07-05 20:56:52 -0400

    Add ImGuiSupport and Fix SDL

---
+++ src/BigNumber.cpp
@@ -0,0 +1,5 @@
+#include "BigNumber.hpp"
+
+BigNumber::BigNumber()
+{
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,76 @@
 #include <iostream>
 
-int main(int, char**){
-    std::cout << "Hello, from triangles!\n";
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h> // What a Include does is get the code in here XD So we are just adding the SDL main code into this file and creating all the functions here
+#include "imgui.h"
+#include "imgui_impl_sdl3.h"
+#include "imgui_impl_sdlrenderer3.h"
+
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
+
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv)
+{
+
+    // Create Window
+    if (!SDL_CreateWindowAndRenderer("SDL Camera", 800, 600, 0, &window, &renderer))
+    {
+        SDL_Log("Coudn't Create Window or Renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+
+    ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer3_Init(renderer);
+
+    return SDL_APP_CONTINUE;
 }
+
+SDL_AppResult SDL_AppIterate(void* appstate)
+{
+    ImGui_ImplSDLRenderer3_NewFrame();
+    ImGui_ImplSDL3_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::Begin("Hello Wolrd!");
+    ImGui::Text("Welcome To ImGui");
+    ImGui::End();
+    // ImGui::ShowDemoWindow(nullptr);
+
+    ImGui::Render();
+    
+    SDL_SetRenderDrawColorFloat(renderer, 0.4f, 0.6f, 1.0f, SDL_ALPHA_OPAQUE_FLOAT);
+    SDL_RenderClear(renderer);
+
+    ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
+
+    SDL_RenderPresent(renderer);
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
+{
+    ImGui_ImplSDL3_ProcessEvent(event);
+    if (event->type == SDL_EVENT_QUIT)
+    {
+        return SDL_APP_SUCCESS;
+
+    }
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+}
+
